a0d8e2503e4b65b50e0d32a0adb0ccde04cde069
isaiahdebenedetto <isaiah@dopplercreative.com>
2019-09-08 16:11:45 -0700

    add linked list function

--- 2darray/2darray.cpp
+++ 2darray/2darray.cpp
@@ -3,6 +3,48 @@ using namespace std;
 
 int main()
 {	
+	
+	const int rows = 5;
+	const int cols = 5;
+	const int tileSize = 3;
+	string mapString = "";
+	int type;
+	int twoDimensionalArray[rows][cols] = { 
+		{2,1,0,0,0}, 
+		{0,0,0,1,0},
+		{1,1,1,1,0},
+		{1,0,0,0,0},
+		{1,3,1,1,1}
+	};
+	
+	for (int row = 0; row < rows; row++) { // to iterate through the rows of the maze
+		for (int a = 0; a < tileSize; a++) { // to create the 3x3 tilesize
+			for (int col = 0; col < cols; col++) { // to iterate throught the columns of the maze
+				for (int b = 0; b < tileSize; b++) { // to create the 3x3 tilesize
+					type = twoDimensionalArray[row][col]; // set type to equal the value of index
+					if (type == 0) { // create path
+						mapString += ".";
+					}
+					else if (type == 2) { // start
+						if (a == 1 && b == 1) mapString += "S"; // center point
+						else mapString += ".";
+					}
+					else if (type == 3) { // finish
+						if (a == 1 && b == 1) mapString += "F"; // center point
+						else mapString += ".";
+					}
+					else { // create walls
+						mapString += "x";
+					}
+				}
+			}
+			mapString += "\n"; // create line break
+		}
+	}
+	cout << mapString;
+
+	
+	/*
 	const int rows = 5;
 	const int cols = 5;
 	const int tileSize = 3;
@@ -41,4 +83,5 @@ int main()
 		}
 	}
 	cout << mapString;
+	*/
 }
---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+To run this program I used Visual Studio Code Community 
+2019. When running make sure to select the current 
+document to run.
